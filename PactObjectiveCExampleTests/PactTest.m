3f099fc18df052152f5fad69551463037a29aeae
Andrew Spinks <andrewspinks@gmail.com>
2015-02-12 13:27:09 +0900

    Update to simpler syntax.

--- PactObjectiveCExampleTests/PactTest.m
+++ PactObjectiveCExampleTests/PactTest.m
@@ -14,7 +14,11 @@
 
 - (void)setUp {
   [super setUp];
-  self.mockService = [[MockService alloc] initWithProvider:@"Provider" consumer:@"consumer"];
+  XCTestExpectation *exp = [self expectationWithDescription:@"Pacts all verified"];
+  self.mockService = [[MockService alloc] initWithProvider:@"Provider" consumer:@"consumer" done:^(PactVerificationResult result) {
+    XCTAssert(result == PactVerificationResultPassed);
+    [exp fulfill];
+  }];
   self.helloClient = [[HelloClient alloc] initWithBaseUrl:self.mockService.baseUrl];
 }
 
@@ -24,20 +28,14 @@
 
 - (void)testPact {
   typedef void (^CompleteBlock)();
-  XCTestExpectation *exp = [self expectationWithDescription:@"Responds with hello"];
-  
   [[[self.mockService uponReceiving:@"a request for hello"]
                  withRequest:1 path:@"/sayHello" query:nil headers:nil body: nil]
                  willRespondWith:200 headers:@{@"Content-Type": @"application/json"} body: @"Hello" ];
   
-  [[self.mockService run:^(CompleteBlock testComplete) {
+  [self.mockService run:^(CompleteBlock testComplete) {
       NSString *requestReply = [self.helloClient sayHello];
       XCTAssertEqualObjects(requestReply, @"Hello");
       testComplete();
-    }
-  ] onComplete:^(PactVerificationResult result) {
-     XCTAssert(result == PactVerificationResultPassed);
-     [exp fulfill];
     }
   ];
   
@@ -46,20 +44,15 @@
 
 - (void)testWithQueryParams {
   typedef void (^CompleteBlock)();
-  XCTestExpectation *exp = [self expectationWithDescription:@"Responds with matching friends"];
   
   [[[self.mockService uponReceiving:@"a request friends"]
     withRequest:1 path:@"/friends" query: @{ @"age" : @"30", @"child" : @"Mary" } headers:nil body: nil]
    willRespondWith:200 headers:@{@"Content-Type": @"application/json"} body: @{ @"friends": @[ @"Sue" ] } ];
   
-  [[self.mockService run:^(CompleteBlock testComplete) {
+  [self.mockService run:^(CompleteBlock testComplete) {
       NSString *response = [self.helloClient findFriendsByAgeAndChild];
       XCTAssertEqualObjects(response, @"{\"friends\":[\"Sue\"]}");
       testComplete();
-    }
-  ] onComplete:^(PactVerificationResult result) {
-      XCTAssert(result == PactVerificationResultPassed);
-      [exp fulfill];
     }
   ];
   
@@ -68,7 +61,6 @@
 
 - (void)testExpectedError {
   typedef void (^CompleteBlock)();
-  XCTestExpectation *exp = [self expectationWithDescription:@"Respons with successful unfriending"];
   
   [[[[self.mockService
       given: @"I have no friends" ]
@@ -76,17 +68,13 @@
     withRequest:PactHTTPMethodPut path:@"/unfriendMe" query: nil headers:nil body: nil]
    willRespondWith:404 headers:nil body: nil ];
   
-  [[self.mockService run:^(CompleteBlock testComplete) {
+  [self.mockService run:^(CompleteBlock testComplete) {
       [self.helloClient unfriend:^(NSString *response) {
                           XCTAssertFalse(true);
                         } failure:^(NSInteger errorCode) {
                           XCTAssertEqual(errorCode, 404);
                         }];
       testComplete();
-    }
-  ] onComplete:^(PactVerificationResult result) {
-      XCTAssert(result == PactVerificationResultPassed);
-      [exp fulfill];
     }
   ];
   
