27854ee4e6e8773353ec9e1e27fe76a815739e74
Andrew Spinks <andrewspinks@gmail.com>
2015-02-27 14:12:42 +0900

    Update to new interface.

--- PactObjectiveCExampleTests/PactTest.m
+++ PactObjectiveCExampleTests/PactTest.m
@@ -29,8 +29,8 @@
 - (void)testPact {
   typedef void (^CompleteBlock)();
   [[[self.mockService uponReceiving:@"a request for hello"]
-                 withRequest:1 path:@"/sayHello" query:nil headers:nil body: nil]
-                 willRespondWith:200 headers:@{@"Content-Type": @"application/json"} body: @"Hello" ];
+                      withRequestHTTPMethod:PactHTTPMethodGET path:@"/sayHello" query:nil headers:nil body: nil]
+                      willRespondWithHTTPStatus:200 headers:@{@"Content-Type": @"application/json"} body: @"Hello" ];
   
   [self.mockService run:^(CompleteBlock testComplete) {
       NSString *requestReply = [self.helloClient sayHello];
@@ -46,34 +46,12 @@
   typedef void (^CompleteBlock)();
   
   [[[self.mockService uponReceiving:@"a request friends"]
-    withRequest:1 path:@"/friends" query: @{ @"age" : @"30", @"child" : @"Mary" } headers:nil body: nil]
-   willRespondWith:200 headers:@{@"Content-Type": @"application/json"} body: @{ @"friends": @[ @"Sue" ] } ];
+                      withRequestHTTPMethod:PactHTTPMethodGET path:@"/friends" query: @{ @"age" : @"30", @"child" : @"Mary" } headers:nil body: nil]
+                      willRespondWithHTTPStatus:200 headers:@{@"Content-Type": @"application/json"} body: @{ @"friends": @[ @"Sue" ] } ];
   
   [self.mockService run:^(CompleteBlock testComplete) {
       NSString *response = [self.helloClient findFriendsByAgeAndChild];
       XCTAssertEqualObjects(response, @"{\"friends\":[\"Sue\"]}");
-      testComplete();
-    }
-  ];
-  
-  [self waitForExpectationsWithTimeout:5 handler:nil];
-}
-
-- (void)testExpectedError {
-  typedef void (^CompleteBlock)();
-  
-  [[[[self.mockService
-      given: @"I have no friends" ]
-     uponReceiving:@"a request to unfriend"]
-    withRequest:PactHTTPMethodPut path:@"/unfriendMe" query: nil headers:nil body: nil]
-   willRespondWith:404 headers:nil body: nil ];
-  
-  [self.mockService run:^(CompleteBlock testComplete) {
-      [self.helloClient unfriend:^(NSString *response) {
-                          XCTAssertFalse(true);
-                        } failure:^(NSInteger errorCode) {
-                          XCTAssertEqual(errorCode, 404);
-                        }];
       testComplete();
     }
   ];
