8c02af8fbbea33ab9c596a9f39388ad8571430e0
Andrew Spinks <andrewspinks@gmail.com>
2016-09-28 11:23:20 +1000

    Update for swift 3.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -o pipefail
 
-xcodebuild -workspace PactObjectiveCExample.xcworkspace -scheme PactObjectiveCExample clean test -destination 'platform=iOS Simulator,OS=10.0,name=iPhone 6' -sdk iphonesimulator | xcpretty -c
+scan
